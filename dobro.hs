641ea81cea51996ab83c05fd23c72be471b5ebbb
Thiago Moura <thiago.moura@ccc.ufcg.edu.br>
2018-09-05 23:58:45 -0300

    first commit

---
+++ dobro.hs
@@ -0,0 +1,7 @@
+doubleMe :: Int -> Int
+doubleMe x = x * 2
+
+main = do  
+    input <- getLine
+    let num = doubleMe (read input)
+    print num
---
+++ dobro.pl
@@ -0,0 +1,10 @@
+:- initialization main.
+
+main:-
+    repeat,
+    read_line_to_codes(user_input, X3),
+    string_to_atom(X3,X2),
+    atom_number(X2,X),
+    Y is X * 2,
+    write(Y),nl,
+    halt(0).
