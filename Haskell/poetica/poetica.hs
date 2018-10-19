3366c14af7f33c18ab95c63d676584dcda7bdd08
Thiago Santos <thiago.moura@ccc.ufcg.edu.br>
2018-10-19 14:06:10 -0300

    Subindo poetica funcionando

--- Haskell/poetica/poetica.hs
+++ Haskell/poetica/poetica.hs
@@ -1,14 +1,9 @@
-ehpoetica :: [Char] -> [Char] -> Char
-ehpoetica array1 array2
-    | (palavra1) == (palavra2) = 'S'
-    | otherwise = 'N'
-    where 
-        palavra1 = map (:[]) array1
-        palavra2 = map (:[]) array2
+saoPoeticas :: IO()
+saoPoeticas = do
+	a <- getLine
+	b <- getLine
+	if head a == head b && last a == last b then putStrLn ("S")
+	else putStrLn ("N")
 
-main :: IO()
-main = do  
-    input1 <- getLine
-    input2 <- getLine
-    let retorno = ehpoetica (read input1) (read input2)
-    print retorno
+main = do
+	saoPoeticas
